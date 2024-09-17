6966da8b999539f3eeab7a3ff22806301bc2c779
Simon Harwick <harwick@hdm.edu>
2024-09-17 11:49:24 +0200

    refactor: changed required length of password to 1 to align with other applications

--- c-sharp-razor/src/Minitwit.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ c-sharp-razor/src/Minitwit.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -98,7 +98,7 @@ namespace Minitwit.Web.Areas.Identity.Pages.Account
             [StringLength(
                 100,
                 ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
-                MinimumLength = 6
+                MinimumLength = 1
             )]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
