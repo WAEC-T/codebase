b50c96a49d1113b21ea9f85c4f1fbb1b9ee25dda
Simon Harwick <harwick@hdm.edu>
2024-09-25 11:04:53 +0200

    feat: added simulator endpoints for python flask

--- python-flask/app/utils.py
+++ python-flask/app/utils.py
@@ -1,12 +1,16 @@
 import hashlib
 from datetime import datetime
 from flask import g
+from flask import jsonify
+from app.models.user import User
+from app.models.latest import Latest
+from extensions import db
+
 
 def get_user_id(username):
     """Convenience method to look up the id for a username."""
-    rv = g.db.execute('select user_id from user where username = ?',
-                      [username]).fetchone()
-    return rv[0] if rv else None
+    user = User.query.filter_by(username=username).first()
+    return user.user_id if user else None
 
 
 def format_datetime(timestamp):
@@ -25,3 +29,23 @@ def gravatar(email, size=80):
     """Return the gravatar image for the given email address."""
     return 'http://www.gravatar.com/avatar/%s?d=identicon&s=%d' % \
         (hashlib.md5(email.strip().lower().encode('utf-8')).hexdigest(), size)
+
+
+def not_req_from_simulator(request):
+    from_simulator = request.headers.get("Authorization")
+    if from_simulator != "Basic c2ltdWxhdG9yOnN1cGVyX3NhZmUh":
+        error = "You are not authorized to use this resource!"
+        return jsonify({"status": 403, "error_msg": error}), 403
+
+
+def update_latest(request):
+    """Update the latest processed command id"""
+    parsed_command_id = request.args.get("latest", type=int)
+    if parsed_command_id is not None:
+        latest_entry = Latest.query.first()
+        if latest_entry:
+            latest_entry.value = parsed_command_id
+        else:
+            latest_entry = Latest(id=1, value=parsed_command_id)
+            db.session.add(latest_entry)
+        db.session.commit()  # Save changes
