5d844386e0769e69a178a3142695d6937980b827
dudustri <eduardostrindade@gmail.com>
2024-10-06 10:49:58 +0200

    refactor: changes main and server

--- rust-actix/src/server.rs
+++ rust-actix/src/server.rs
@@ -1,7 +1,6 @@
 use actix_web::middleware::Logger;
-use actix_web::{App, HttpServer, Scope, web};
-use std::collections::HashMap;
-use crate::api::routes::*;
+use actix_web::{App, HttpServer};
+use mini_x::api::services::api_services;
 
 // TODO: right now it has only the api ! Add all the frontend as a service here too! [figure out how to not use the middleware and stuff fr the api]
 #[actix_web::main]
@@ -9,7 +8,7 @@ pub async fn start() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
-            .configure(api_services)
+            .service(api_services())
     })
     .bind(("0.0.0.0", 5000))?
     .run()
