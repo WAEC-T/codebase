7cea49df6dfc4768dbf8bf0c32c654401c84dd1e
Simon Harwick <simon.harwick97@gmail.com>
2024-10-14 16:35:22 +0200

    feat: added Dockerfile for build and docker-compose file

--- c-sharp-razor/src/Minitwit.Web/Program.cs
+++ c-sharp-razor/src/Minitwit.Web/Program.cs
@@ -6,100 +6,79 @@ using Minitwit.Infrastructure;
 using Minitwit.Infrastructure.Repository;
 using Minitwit.Web;
 
-/// <summary>
-/// This file is the entry point of the application.
-/// It is responsible for setting up the application and starting it.
-/// </summary>
-
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-ProgramOptions.AddProgramOptions(builder);
-ProgramOptions.AddIdendity(builder);
-ProgramOptions.AddDatabase(builder);
-
-//API Controllers
-builder
-    .Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-        options.JsonSerializerOptions.PropertyNamingPolicy = null;
-        options.JsonSerializerOptions.IgnoreNullValues = true;
-    });
-
-// TODO put do this in ProgramOptions
-//Client that prometheus uses to report metric
-//Src: https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.Prometheus.AspNetCore/README.md
-
-// Dependency Injection
-builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
-builder.Services.AddScoped<IValidator<CreateCheep>, CheepCreateValidator>();
-builder.Services.AddScoped<ICheepRepository, CheepRepository>();
-builder.Services.AddScoped<ICheepService, MinitwitService>();
-builder.Services.AddScoped<IFollowRepository, FollowRepository>();
-builder.Services.AddScoped<ILatestRepository, LatestRepository>();
-
-builder.Services.AddSession(options =>
+public class Program
 {
-    options.Cookie.Name = ".Minitwit.Web.Session";
-    options.IdleTimeout = TimeSpan.FromMinutes(10);
-    options.Cookie.HttpOnly = false;
-    options.Cookie.IsEssential = true;
-});
-
-WebApplication app = builder.Build();
-
-using (IServiceScope scope = app.Services.CreateScope())
-{
-    IServiceProvider services = scope.ServiceProvider;
-    MinitwitDbContext context = services.GetRequiredService<MinitwitDbContext>();
-
-    try
+    public static void Main(string[] args)
     {
-        context.Database.Migrate();
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e);
-    }
-}
+        var builder = WebApplication.CreateBuilder(args);
 
-// DB migrations for ASP.NET Identity
-using (IServiceScope scope = app.Services.CreateScope())
-{
-    IServiceProvider services = scope.ServiceProvider;
-    MinitwitDbContext context = services.GetRequiredService<MinitwitDbContext>();
+        // Add services to the container.
+        builder.Services.AddRazorPages();
 
-    try
-    {
-        context.Database.Migrate();
-        Console.WriteLine("Database migration applied successfully.");
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e);
-        Console.WriteLine($"Database migration failed with error.{e.Message}");
+        ProgramOptions.AddProgramOptions(builder);
+        ProgramOptions.AddIdendity(builder);
+        ProgramOptions.AddDatabase(builder);
+
+        //API Controllers
+        builder
+            .Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                options.JsonSerializerOptions.IgnoreNullValues = true;
+            });
+
+        // Dependency Injection
+        builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+        builder.Services.AddScoped<IValidator<CreateCheep>, CheepCreateValidator>();
+        builder.Services.AddScoped<ICheepRepository, CheepRepository>();
+        builder.Services.AddScoped<ICheepService, MinitwitService>();
+        builder.Services.AddScoped<IFollowRepository, FollowRepository>();
+        builder.Services.AddScoped<ILatestRepository, LatestRepository>();
+
+        builder.Services.AddSession(options =>
+        {
+            options.Cookie.Name = ".Minitwit.Web.Session";
+            options.IdleTimeout = TimeSpan.FromMinutes(10);
+            options.Cookie.HttpOnly = false;
+            options.Cookie.IsEssential = true;
+        });
+
+        var app = builder.Build();
+
+        // Apply migrations
+        using (var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<MinitwitDbContext>();
+
+            try
+            {
+                context.Database.Migrate();
+                Console.WriteLine("Database migration applied successfully.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseSession();
+        app.MapControllers();
+        app.MapRazorPages();
+
+        app.Run();
     }
 }
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
-/* ----------- Middleware ----------- */
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseSession();
-app.MapControllers();
-app.MapRazorPages();
-
-app.Run();
