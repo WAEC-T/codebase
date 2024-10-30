81fe8f1df7b12995f7f6b3fef1555388b05b1fae
Simon Harwick <simon.harwick97@gmail.com>
2024-10-30 12:03:56 +0100

    testing with rebase on main

--- python-flask/config.py
+++ python-flask/config.py
@@ -1,5 +1,5 @@
 import os
-# test push cd in python-flask 10. Files has changed test files changes
+# test push cd in python-flask 11. Files has changed test files changes
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
