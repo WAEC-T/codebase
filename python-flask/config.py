d8990fce2d4e451da80c5245b41f6b38f9c8615e
Simon Harwick <simon.harwick97@gmail.com>
2024-11-03 13:02:28 +0100

    test

--- python-flask/config.py
+++ python-flask/config.py
@@ -1,5 +1,5 @@
 import os
-# test run build process without push to docker
+# test run build process without push to docker Testrun 2
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
