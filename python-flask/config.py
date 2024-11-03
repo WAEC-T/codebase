4b9d3e02d70845582a8fa6d58c343a24a8d66157
Simon Harwick <simon.harwick97@gmail.com>
2024-11-03 16:50:34 +0100

    run with check if Dockerfile exist

--- python-flask/config.py
+++ python-flask/config.py
@@ -1,5 +1,5 @@
 import os
-# test run build process without push to docker Testrun 11 - run with aws secrets
+# test run build process without push to docker Testrun 12 - run with aws secrets
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
