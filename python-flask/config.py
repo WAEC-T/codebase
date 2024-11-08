c48d5430d1bc32b737d45511c79ff51df18c8fde
Simon Harwick <simon.harwick97@gmail.com>
2024-11-08 17:46:03 +0100

    test run two

--- python-flask/config.py
+++ python-flask/config.py
@@ -1,5 +1,5 @@
 import os
-# test for arm7
+# test for arm7 - test 2
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
