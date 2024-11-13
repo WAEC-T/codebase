5c77b0135f8a2a55d56c2d662ba5f363ff74c041
Simon Harwick <simon.harwick97@gmail.com>
2024-11-13 10:09:05 +0100

    changes in the set configuration

--- python-flask/config.py
+++ python-flask/config.py
@@ -1,5 +1,5 @@
 import os
-# test build for ci cd pipeline 9
+# test build for ci cd pipeline 10
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
