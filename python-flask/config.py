120810ae482f5e5c182d55f70851455019d66c87
Simon Harwick <simon.harwick97@gmail.com>
2024-11-13 10:42:50 +0100

    adding separator

--- python-flask/config.py
+++ python-flask/config.py
@@ -1,5 +1,5 @@
 import os
-# test build for ci cd pipeline 16
+# test build for ci cd pipeline 17
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
