ed6314590c518e13927ec6c5622eb987a673a6f4
Simon Harwick <simon.harwick97@gmail.com>
2024-11-13 11:12:06 +0100

    fetch depth

--- python-flask/config.py
+++ python-flask/config.py
@@ -1,5 +1,5 @@
 import os
-# test build for ci cd pipeline 23
+# test build for ci cd pipeline 24
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
