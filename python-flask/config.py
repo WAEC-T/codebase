df3185cdf0df02ca56a64b966f94d9c4a03d2848
Simon Harwick <simon.harwick97@gmail.com>
2024-11-13 16:23:17 +0100

    test reusable workflow

--- python-flask/config.py
+++ python-flask/config.py
@@ -1,5 +1,5 @@
 import os
-# test build for ci cd pipeline 47
+# test build for ci cd pipeline 48
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
