404d3ffc1a1733686c2dbd29708176bcb951d98b
Simon Harwick <simon.harwick97@gmail.com>
2024-11-13 13:19:16 +0100

    makefile testrun

--- python-flask/config.py
+++ python-flask/config.py
@@ -1,5 +1,5 @@
 import os
-# test build for ci cd pipeline 30
+# test build for ci cd pipeline 33
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
